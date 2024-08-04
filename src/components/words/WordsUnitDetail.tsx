6bb384c485824de7e4b6ed9d20f796c1a3405332
趙偉 <zwvista@msn.com>
2024-08-04 19:34:18 +0900

    function 3

--- src/components/words/WordsUnitDetail.tsx
+++ src/components/words/WordsUnitDetail.tsx
@@ -7,89 +7,88 @@ import 'reflect-metadata';
 import { container } from "tsyringe";
 import { SettingsService } from '../../view-models/misc/settings.service';
 import { Dropdown } from 'primereact/dropdown';
+import { useNavigate, useParams } from "react-router-dom";
+import { useReducer } from "react";
+import { InputNumber } from "primereact/inputnumber";
 
-export default class WordsUnitDetail extends React.Component<any, any> {
-  wordsUnitService = container.resolve(WordsUnitService);
-  settingsService = container.resolve(SettingsService);
+export default function WordsUnitDetail() {
+  const wordsUnitService = container.resolve(WordsUnitService);
+  const settingsService = container.resolve(SettingsService);
+  const navigate = useNavigate();
+  const params = useParams();
+  const item = (() => {
+    const id = +params.id;
+    const o = wordsUnitService.unitWords.find(value => value.ID === id);
+    return o || wordsUnitService.newUnitWord();
+  })();
+  const [, forceUpdate] = useReducer(x => x + 1, 0);
 
-  componentDidMount() {
-    const id = +this.props.match.params.id;
-    const o = this.wordsUnitService.unitWords.find(value => value.ID === id);
-    this.setState({
-      item: o || this.wordsUnitService.newUnitWord(),
-    });
-  }
-
-  render() {
-    return this.state && (
-      <div>
-        <div className="p-grid mt-2 mb-2">
-          <label className="p-col-1" htmlFor="ID">ID:</label>
-          <InputText className="p-col-3" id="ID" value={this.state.item.ID} disabled />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="UNIT">UNIT:</label>
-          <Dropdown className="p-col-3" id="UNIT" options={this.settingsService.units} value={this.state.item.UNIT} onChange={this.onChangeDropDown} />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="PART">PART:</label>
-          <Dropdown className="p-col-3" id="PART" options={this.settingsService.parts} value={this.state.item.PART} onChange={this.onChangeDropDown} />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="SEQNUM">SEQNUM:</label>
-          <InputText className="p-col-3" id="SEQNUM" value={this.state.item.SEQNUM} onChange={this.onChangeInput} />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="WORDID">WORDID:</label>
-          <InputText className="p-col-3" id="WORDID" value={this.state.item.WORDID} disabled />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="WORD">WORD:</label>
-          <InputText className="p-col-3" id="WORD" value={this.state.item.WORD} onChange={this.onChangeInput} />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="NOTE">NOTE:</label>
-          <InputText className="p-col-3" id="NOTE" value={this.state.item.NOTE} onChange={this.onChangeInput} />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="FAMIID">FAMIID:</label>
-          <InputText className="p-col-3" id="FAMIID" value={this.state.item.FAMIID} disabled />
-        </div>
-        <div className="p-grid mb-2">
-          <label className="p-col-1" htmlFor="ACCURACY">ACCURACY:</label>
-          <InputText className="p-col-3" id="ACCURACY" value={this.state.item.ACCURACY} disabled />
-        </div>
-        <div>
-          <Button label="Back" onClick={this.goBack} />
-          <Button label="Save" onClick={this.save} />
-        </div>
-      </div>
-    );
-  }
-
-  onChangeInput = (e: any) => {
+  const onChangeInput = (e: any) => {
     const elem = e.nativeEvent.target as HTMLInputElement;
-    this.state.item[elem.id] = elem.value;
-    this.setState({item: this.state.item})
+    item[elem.id] = elem.value;
+    forceUpdate();
   };
 
-  onChangeDropDown = (e: any) => {
-    this.state.item[e.target.id] = e.value;
-    this.setState({item: this.state.item})
+  const onChangeDropDown = (e: any) => {
+    item[e.target.id] = e.value;
+    forceUpdate();
   };
 
-  goBack = () => {
-    history.back();
+  const goBack = () => {
+    navigate(-1);
   };
 
-  save = async () => {
-    this.state.item.WORD = this.settingsService.autoCorrectInput(this.state.item.WORD);
-    if (this.state.item.ID)
-      await this.wordsUnitService.update(this.state.item);
+  const save = async () => {
+    item.WORD = settingsService.autoCorrectInput(item.WORD);
+    if (item.ID)
+      await wordsUnitService.update(item);
     else
-      await this.wordsUnitService.create(this.state.item)
-    this.goBack();
+      await wordsUnitService.create(item)
+    goBack();
   };
 
-};
-
+  return (
+    <div>
+      <div className="p-grid mt-2 mb-2">
+        <label className="p-col-1" htmlFor="ID">ID:</label>
+        <InputNumber className="p-col-3" id="ID" value={item.ID} disabled />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="UNIT">UNIT:</label>
+        <Dropdown className="p-col-3" id="UNIT" options={settingsService.units} value={item.UNIT} onChange={onChangeDropDown} />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="PART">PART:</label>
+        <Dropdown className="p-col-3" id="PART" options={settingsService.parts} value={item.PART} onChange={onChangeDropDown} />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="SEQNUM">SEQNUM:</label>
+        <InputNumber className="p-col-3" id="SEQNUM" value={item.SEQNUM} onChange={onChangeInput} />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="WORDID">WORDID:</label>
+        <InputNumber className="p-col-3" id="WORDID" value={item.WORDID} disabled />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="WORD">WORD:</label>
+        <InputText className="p-col-3" id="WORD" value={item.WORD} onChange={onChangeInput} />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="NOTE">NOTE:</label>
+        <InputText className="p-col-3" id="NOTE" value={item.NOTE} onChange={onChangeInput} />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="FAMIID">FAMIID:</label>
+        <InputNumber className="p-col-3" id="FAMIID" value={item.FAMIID} disabled />
+      </div>
+      <div className="p-grid mb-2">
+        <label className="p-col-1" htmlFor="ACCURACY">ACCURACY:</label>
+        <InputText className="p-col-3" id="ACCURACY" value={item.ACCURACY} disabled />
+      </div>
+      <div>
+        <Button label="Back" onClick={goBack} />
+        <Button label="Save" onClick={save} />
+      </div>
+    </div>
+  );
+}
